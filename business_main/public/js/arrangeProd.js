8970153cf1b4c910c7a155a8927bb6a1e547ca36
phuongvuong98 <phuongvuong98@gmail.com>
2019-04-18 04:17:37 +0000

    Divide multi-category

---
+++ business_main/public/js/arrangeProd.js
@@ -0,0 +1,26 @@
+(function ($) {
+  // $(".price_LH").click(function(){
+  // 	console.log("products[0]:", products[0]);	
+  //     // var arrTrans = [];
+  //     // $(".translate").each(function() {
+  //     //     arrTrans.push($(this).text().trim());
+  //     // });
+
+  //     // stringTrans = arrTrans.join(",");
+  //     // console.log("stringTrans:", stringTrans);
+  //     // const translate = require("translate");
+  //     // const bar = translate(stringTrans, { from: 'en', to: 'vi', engine: 'google', key: 'AIzaSyA9oA2ivdWXD9aMEGdKLMIPFv3A_Vff2Ms' })
+  //     // .then(result => {
+  //     //     console.log("GET TRANSLATE!");
+  //     //     console.log("result:", result);
+  //     //     const arr = result.split(",");
+  //     //     console.log("arr", arr);
+
+  //     //     $(".translate").each(function(index) {
+  //     //         console.log(arr[index]);
+  //     //         $(".translate")[index].innerHTML = arr[index];
+  //     //     });
+  //     // });
+  // });
+
+})(jQuery);
