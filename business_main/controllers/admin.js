3ed21296d3525d515067c29ea41fc02d2a073786
NguyenPhuongVuong <nguyenphuongvuong@vuongmb.local>
2019-04-27 12:27:54 +0700

    edit add product admin

--- business_main/controllers/admin.js
+++ business_main/controllers/admin.js
@@ -3,16 +3,17 @@ const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
   const product = new Product({
-    name: "Summer clothes",
-    category: "women",
-    price: "199",
-    size: ["XL", "M", "L"],
-    name: "Sping clothes",
-    category: "women",
+    name: "Bag " + i.toString(),
+    category: "bag",
     imageUrl:"https://ucarecdn.com/9a56e72d-841b-4cb2-8ef2-688fee9b4baf/product-01.jpg",
+    price: "120",
+    size:["XL","S","M","L"],
+    description: "huhu",
     userId: req.user
   });
   product.save();
+  console.log("Add get sucessfully");
+
   res.redirect("/");
 };
 
